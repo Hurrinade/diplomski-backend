41e63263c82cdfde9ef94d6c4e0478194ead2401
Marko Uremović <marko.uremovic00@gmail.com>
2024-02-11 11:41:16 +0100

    little events rework

--- controller/events.go
+++ controller/events.go
@@ -11,34 +11,35 @@ import (
 )
 
 func GetEvents(ctx *gin.Context, apiDetails ApiDetails) {
-
-
 	gone := ctx.Stream(func(w io.Writer) bool {
-		weatherDataResponse, err := http.Get(apiDetails.PljusakURL)
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if weatherDataResponse.StatusCode == http.StatusOK {
-			eventData := FormatData("pljusak", weatherDataResponse.Body)
-			ctx.SSEvent("data", eventData)
-		} else {
-			weatherDataResponse, err = http.Get(apiDetails.WuURL)
-	
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			eventData := FormatData("wu", weatherDataResponse.Body)
-			ctx.SSEvent("data", eventData)
-		}
-
-		time.Sleep(5 * time.Second)
+		sendEvent(ctx, apiDetails)
+		time.Sleep(15 * time.Second)
 		return true
 	})
 
 	if gone {
 		log.Println("Client disconected")
+	}
+}
+
+func sendEvent(ctx *gin.Context, apiDetails ApiDetails) {
+	weatherDataResponse, err := http.Get(apiDetails.PljusakURL)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if weatherDataResponse.StatusCode == http.StatusOK {
+		eventData := FormatData("pljusak", weatherDataResponse.Body)
+		ctx.SSEvent("data", eventData)
+	} else {
+		weatherDataResponse, err = http.Get(apiDetails.WuURL)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		eventData := FormatData("wu", weatherDataResponse.Body)
+		ctx.SSEvent("data", eventData)
 	}
 }
